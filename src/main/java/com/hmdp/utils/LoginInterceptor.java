386ad90676e240011c84962cd3823fe5b49fabbc
Nick <nickzhangjy@126.com>
2025-03-21 19:59:10 -0400

    短信登录模块代码开发

--- src/main/java/com/hmdp/utils/LoginInterceptor.java
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -1,6 +1,33 @@
 package com.hmdp.utils;
 
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public class LoginInterceptor implements HandlerInterceptor {
+
+    private StringRedisTemplate stringRedisTemplate;
+
+    public LoginInterceptor() {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //判断是否需要拦截（ThreadLocal中是否有用户）
+        if(UserHolder.getUser() == null) {
+            //没有，需要拦截
+            response.setStatus(401);
+            return false;
+        }
+        //有用户，放行
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        UserHolder.removeUser();
+    }
 }
